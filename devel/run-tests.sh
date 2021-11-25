d353e61e1aca8ac2c8bbc33c951b8d2905ad71b5
Inteon <42113979+inteon@users.noreply.github.com>
2021-11-25 12:34:45 +0100

    improve GH
    Signed-off-by: Inteon <42113979+inteon@users.noreply.github.com>

--- devel/run-tests.sh
+++ devel/run-tests.sh
@@ -3,4 +3,8 @@
 ROOT="$(dirname $(dirname $0))"
 
 cd "$ROOT"
-pytest -n auto --cov --cov-branch --cov-report html "tests/" $@
+pytest -n auto \
+    --cov \
+    --cov-branch \
+    --cov-report html \
+    "tests/" $@
